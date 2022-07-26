6ffea928107330849f5e29e13733fbabe54bb17e
Ricky Indra Gunawan <rickyindra006@gmail.com>
2022-07-26 14:02:17 +0700

    add close modal button

--- components/FormModal.js
+++ components/FormModal.js
@@ -1,5 +1,6 @@
 import { Formik } from "formik";
-import { View, TextInput, Button } from "react-native";
+import { View, TextInput, Button, TouchableOpacity, TouchableWithoutFeedback, Keyboard } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
 import tailwind from "twrnc";
 import { useDispatch } from "react-redux";
 import { addNoteAsync } from "../features/noteSlice";
@@ -16,18 +17,23 @@ export default function FormModal({ setModalVisible }) {
     }
   };
   return (
-    <View style={tailwind`flex-1 justify-center bg-slate-200/80 items-center`}>
-      <View style={tailwind`bg-slate-200 p-4 w-[70] h-[76] rounded-md`}>
-        <Formik initialValues={{ title: "", content: "" }} onSubmit={(values, { resetForm }) => handleFormSubmit(values, resetForm)}>
-          {({ handleChange, handleBlur, handleSubmit, values }) => (
-            <View style={tailwind`flex-1`}>
-              <TextInput style={tailwind`bg-slate-100 h-[10] rounded-md mb-2 px-2`} onChangeText={handleChange("title")} onBlur={handleBlur("title")} value={values.title} />
-              <TextInput style={{ ...tailwind`bg-slate-100 h-[46] rounded-md mb-2 p-2`, textAlignVertical: "top" }} multiline onChangeText={handleChange("content")} onBlur={handleBlur("content")} value={values.content} />
-              <Button onPress={handleSubmit} title="Submit" color={"red"} />
-            </View>
-          )}
-        </Formik>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={tailwind`flex-1 justify-center bg-white/60 items-center`}>
+        <View style={tailwind`bg-slate-200 p-4 w-[70] h-[76] rounded-md`}>
+          <Formik initialValues={{ title: "", content: "" }} onSubmit={(values, { resetForm }) => handleFormSubmit(values, resetForm)}>
+            {({ handleChange, handleBlur, handleSubmit, values }) => (
+              <View style={tailwind`flex-1`}>
+                <TextInput style={tailwind`bg-white h-[10] rounded-md mb-2 px-2`} onChangeText={handleChange("title")} onBlur={handleBlur("title")} value={values.title} />
+                <TextInput style={{ ...tailwind`bg-white h-[46] rounded-md mb-2 p-2`, textAlignVertical: "top" }} multiline onChangeText={handleChange("content")} onBlur={handleBlur("content")} value={values.content} />
+                <Button onPress={handleSubmit} title="Submit" color={"red"} />
+              </View>
+            )}
+          </Formik>
+          <TouchableOpacity onPress={() => setModalVisible(false)} style={{ ...tailwind`absolute right-0 top-0`, transform: [{ translateX: 8 }, { translateY: -8 }] }}>
+            <AntDesign name="closecircle" size={30} color="red" />
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
